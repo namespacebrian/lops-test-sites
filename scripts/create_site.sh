10caeb207a7aae9ab49756c7f9d6d8ddd889bd40
Brian Weaver <brian.weaver@pantheon.io>
2024-03-12 17:54:41 -0400

    More create_site.sh instructions

--- scripts/create_site.sh
+++ scripts/create_site.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+## Install supporting packages
+# brew install tmux parallel gnu-sed
+
+## Fix parallel to work with tmux
+# sed -i.bak 's/\$len = ::min(\$len,int(\$tmux_len\/4-33));/\$len = ::min(\$len,\$tmux_len-33);/' $(readlink -f $(which parallel))
+
+## Create 16 sites named `mytestsites-001` to `mytestsites-016`, running 8 parallel jobs
 # parallel --tmuxpane --fg --delay 0.1 -j 8 ./scripts/create_site.sh mytestsites {} ::: $(seq 1 16)
 
 site_name=$(printf "$1-%03d" $2)
